f6ede997f4d212fbac45ffaf1b0ecec326c80feb
Casey Atnip <caseyatnip08@Caseys-MacBook-Pro-2.local>
2014-03-15 13:39:55 -0700

    First commit with xcode project

---
+++ SocialMappingApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SocialMappingApp
+//
+//  Created by Casey Atnip on 3/15/14.
+//  Copyright (c) 2014 Casey Atnip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SocialMappingApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SocialMappingApp
+//
+//  Created by Casey Atnip on 3/15/14.
+//  Copyright (c) 2014 Casey Atnip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
